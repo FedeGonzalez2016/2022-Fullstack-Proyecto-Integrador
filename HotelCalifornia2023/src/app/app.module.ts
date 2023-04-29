642ffd10262b2e1e368b80be3654fc813d4b32c0
Natalia Moroni <nmoronidamasso@gmail.com>
2023-04-29 01:08:25 -0300

    OTHER: se genera page para la vista de habitaciones

--- HotelCalifornia2023/src/app/app.module.ts
+++ HotelCalifornia2023/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HabitacionesEcommerceComponent } from './pages/habitaciones-ecommerce/habitaciones-ecommerce.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HabitacionesEcommerceComponent
   ],
   imports: [
     BrowserModule,
