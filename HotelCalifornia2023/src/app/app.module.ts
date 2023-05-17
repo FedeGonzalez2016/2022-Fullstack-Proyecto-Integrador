ed8da50c5b7901d9b906b853e5b2bbf07ff917bb
Valen Carrascal <valecarrascal13@gmail.com>
2023-05-17 00:06:45 -0300

    REFACTOR: modularización del proyecto- 17/5

--- HotelCalifornia2023/src/app/app.module.ts
+++ HotelCalifornia2023/src/app/app.module.ts
@@ -3,28 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HabitacionesEcommerceComponent } from './pages/habitaciones-ecommerce/habitaciones-ecommerce.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegistroComponent } from './pages/registro/registro.component';
-import { ConsultaComponent } from './pages/consulta/consulta.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HabitacionesEcommerceComponent,
-    LoginComponent,
-    RegistroComponent,
-    ConsultaComponent,
-    HeaderComponent,
-    FooterComponent,
-    ServiciosComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    SharedModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- HotelCalifornia2023/src/app/shared/shared.module.ts
+++ HotelCalifornia2023/src/app/shared/shared.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { DropdownComponent } from './dropdown/dropdown.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    DropdownComponent
   ],
   imports: [
     CommonModule
