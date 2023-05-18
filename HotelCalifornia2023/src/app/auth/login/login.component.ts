ee6723bbb45cbd50b60e880734f627b229210002
Valen Carrascal <valecarrascal13@gmail.com>
2023-05-18 08:48:08 -0300

    formulario de login funcional para angular

--- HotelCalifornia2023/src/app/auth/login/login.component.ts
+++ HotelCalifornia2023/src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/service/auth/login.service';
+import { LoginRequest } from 'src/app/service/auth/loginRequest';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginError:string="";
+  loginForm=this.formBuilder.group({
+    email:['', [Validators.required, Validators.email]],
+    password:['' ,Validators.required]
+  })
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder, private router:Router, private loginService:LoginService) { }
 
   ngOnInit(): void {
+  }
+  get email(){
+    return this.loginForm.controls.email;
+  }
+  get password (){
+    return this.loginForm.controls.password;
+  }
+
+  login() {
+    if(this.loginForm.valid){
+      this.loginService.login(this.loginForm.value as LoginRequest).subscribe({
+        next: (userData) => {
+           console.log(userData);
+        },
+        error: (errorData) => {
+           console.error(errorData);
+           this.loginError=errorData;
+        },
+        complete: () => {
+          console.info("login Completo");
+          this.router.navigateByUrl('/inicio');
+          this.loginForm.reset();
+        }
+      })
+    }
+     
+    else{
+      this.loginForm.markAllAsTouched();
+      alert("Error al ingresar los datos");
+    }
   }
 
 }
